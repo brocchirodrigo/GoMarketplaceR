81492b3ff5a6c33ec2cb04a6b1a3f112fa0412e8
Rodrigo Brocchi <rodrigobrocchi@MacBook-Pro-de-Rodrigo-Brocchi.local>
2020-12-10 14:50:58 -0300

    Fix images

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -32,7 +32,6 @@ const CartProvider: React.FC = ({ children }) => {
     async function loadProducts(): Promise<void> {
       // TODO LOAD ITEMS FROM ASYNC STORAGE
     }
-
     loadProducts();
   }, []);
 
